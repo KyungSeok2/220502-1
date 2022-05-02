edc3cfc3719f9eff05c1b5c7737b4cffde0d9a88
Kyung Seok <dkffls2@naver.com>
2022-05-02 09:49:38 +0900

    add 0502

---
+++ souce.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Money = 0;
+	Money = 10;
+	cout << Money + 100 << endl;
+	cout << Money - 5 << endl;
+	cout << Money * 20 << endl;
+	cout << Money / 2 << endl;
+
+	return 0;
+
+}
